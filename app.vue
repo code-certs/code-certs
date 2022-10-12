82ec2682b611ce641e524da8acec6c4acd7f78f9
DevDengChao <2325690622@qq.com>
2022-10-13 00:06:17 +0800

    docs: add basic introduction

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>您好, 欢迎使用 <span>Code: Certs</span>.</h1>
+    <h2>简介</h2>
+    <p><span>Code: Certs</span> 是一项 SSL/TLS 证书免运维服务:</p>
+    <ol>
+      <li>它能定期地为您申请免费的 SSL/TLS 证书, 并提供 PEM 格式的证书文件以供下载.</li>
+      <li>如果您愿意, 它还可以将申请到的证书及时部署到需要的地方.</li>
+    </ol>
+    <p>当然, 上述功能都可以在安全的测试模式下进行运行, 您可以申请一张测试用的证书, 并用这张证书来测试所有可能用得到的地方,
+      以便您在部署正式证书到线上前进行预览.</p>
+    <p>所有的资源都在您的掌控之中, 如果您不想继续使用 <span>Code: Certs</span>, 您随时可以撤销您对 <span>Code: Certs</span> 的授权.</p>
+    <hr/>
+    <h2>价格</h2>
+    <p>由于 <span>Code: Certs</span> 的运行需要使用云服务器等资源, 因此它并不是完全免费的. 但是:</p>
+    <ol>
+      <li>Beta 测试期间, 所有的功能都是免费提供的.</li>
+      <li>我们将在 Beta 测试结束前 3 个月公布定价以便给您充足的时间进行决定.</li>
+      <li>Beta 测试结束后, 我们依然会周期性地提供一定的免费额度以便您能继续使用 <span>Code: Certs.</span></li>
+    </ol>
+    <hr/>
+    准备好了么? 点击 <a href="https://console.code-certs.dengchao.fun" target="_blank">登录/注册</a> 以开始使用 <span>Code: Certs</span>
+    <hr/>
+    <p>如你所见, <span>Code: Certs</span> 正处于非常早期的起步阶段, 如果您有任何意见与建议, 不妨在 <a
+        href="https://github.com/code-certs/code-certs/issues" target="_blank">这里</a> 告诉我们.</p>
+    <div>
+      <a href="https://beian.miit.gov.cn/">湘ICP备20013845号</a>
+    </div>
   </div>
 </template>
