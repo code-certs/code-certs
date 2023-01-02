e5c331c5676e33b0c876bb57485c1a4d9fe9613f
DevDengChao <2325690622@qq.com>
2023-01-02 20:54:39 +0800

    build: generate sitemap.xml when building

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/tailwindcss'
-    ]
+        '@nuxtjs/tailwindcss',
+        '@nuxt/content'
+    ],
+    content: {
+        // https://content.nuxtjs.org/api/configuration
+    },
+    nitro: {
+        prerender: {
+            routes: ['/sitemap.xml']
+        }
+    }
 })
---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,19 @@
+import {serverQueryContent} from '#content/server'
+import {SitemapStream, streamToPromise} from 'sitemap'
+
+// https://content.nuxtjs.org/guide/recipes/sitemap
+export default defineEventHandler(async (event) => {
+    // Fetch all documents
+    const docs = await serverQueryContent(event).find()
+    const sitemap = new SitemapStream({
+        hostname: 'https://code-certs.dengchao.fun'
+    })
+    for (const doc of docs) {
+        sitemap.write({
+            url: doc._path,
+            changefreq: 'monthly'
+        })
+    }
+    sitemap.end()
+    return streamToPromise(sitemap)
+})
